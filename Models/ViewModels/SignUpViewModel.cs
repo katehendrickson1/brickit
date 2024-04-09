f16d6d59fe4b3f85534ee38b9ac1e77dd06e2c8c
katehendrickson1 <119885479+katehendrickson1@users.noreply.github.com>
2024-04-09 16:51:45 -0600

    about us page ad sign up edits

--- Models/ViewModels/SignUpViewModel.cs
+++ Models/ViewModels/SignUpViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class SignUpViewModel
     {
-        public IQueryable<User> Users { get; set; }
-        public IQueryable<Customer> Customers { get; set; }
+        public IQueryable<User> users { get; set; } 
+        public IQueryable<Customer> Customer { get; set; } 
     }
 }
