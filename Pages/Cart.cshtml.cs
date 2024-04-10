4811d07c375fd332a5e8c1ed2dec24d31615d0c8
HaileySanders82 <103207122+HaileySanders82@users.noreply.github.com>
2024-04-09 18:45:05 -0600

    trying on a cart

--- Pages/Cart.cshtml.cs
+++ Pages/Cart.cshtml.cs
@@ -1,3 +1,4 @@
+using brickit.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,25 @@ namespace brickit.Pages
 {
     public class CartModel : PageModel
     {
+        private ILegoRepository _repo;
+        public CartModel(ILegoRepository repo) 
+        { 
+            _repo = repo;
+        }
+
+        public Cart? Cart { get; set; }
         public void OnGet()
         {
+        }
+
+        public void OnPost(int product_ID) 
+        {
+            Product prod = _repo.Products
+                .FirstOrDefault(x => x.product_ID == product_ID);
+
+            Cart = new Cart();
+
+            Cart.AddItem(prod, 1);
         }
     }
 }
